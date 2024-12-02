2f1c00a01e50c2d645745201c5d6a7ba4b10332b
Erdem yanık <138898030+erdemyanikk@users.noreply.github.com>
2024-12-02 23:19:43 +0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GentekElektronik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
